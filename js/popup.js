e73d5a68e431ded652a0a2abafc8f14b2a6a35c5
Lily Meng <LilyxMeng@github.com>
2021-03-14 00:54:18 -0500

    commit

---
+++ js/popup.js
@@ -0,0 +1,38 @@
+//design team
+var designTeamModal = document.getElementById("designTeamModal");
+var designTeamBtn = document.getElementById("designTeamBtn");
+var designTeamClose = document.getElementById("designTeamClose");
+
+designTeamBtn.onclick = function() {
+  designTeamModal.style.display = "block";
+}
+
+designTeamClose.onclick = function() {
+  designTeamModal.style.display = "none";
+}
+
+//content team
+var contentTeamModal = document.getElementById("contentTeamModal");
+var contentTeamBtn = document.getElementById("contentTeamBtn");
+var contentTeamClose = document.getElementById("contentTeamClose");
+
+contentTeamBtn.onclick = function() {
+  contentTeamModal.style.display = "block";
+}
+
+contentTeamClose.onclick = function() {
+  contentTeamModal.style.display = "none";
+}
+
+//social media team
+var socialMediaTeamModal = document.getElementById("socialMediaTeamModal");
+var socialMediaTeamBtn = document.getElementById("socialMediaTeamBtn");
+var socialMediaTeamClose = document.getElementById("socialMediaTeamClose");
+
+socialMediaTeamBtn.onclick = function() {
+  socialMediaTeamModal.style.display = "block";
+}
+
+socialMediaTeamClose.onclick = function() {
+  socialMediaTeamModal.style.display = "none";
+}
