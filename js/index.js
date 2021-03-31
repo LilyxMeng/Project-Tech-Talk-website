a441a217a960eb7421cb0cc73fe11e32e981aa2e
Lily Meng <LilyxMeng@github.com>
2021-03-31 14:02:43 -0400

    added posts

--- js/index.js
+++ js/index.js
@@ -50,3 +50,94 @@ perseveranceMankindBtn.onclick = function () {
 perseveranceMankindClose.onclick = function () {
   perseveranceMankindModal.style.display = "none";
 }
+
+// Can We Make UV Light Lisible post
+var UVLightModal = document.getElementById("UVLightModal");
+var UVLightBtn = document.getElementById("UVLightBtn");
+var UVLightClose = document.getElementById("UVLightClose");
+
+UVLightBtn.onclick = function () {
+  UVLightModal.style.display = "block";
+}
+
+UVLightClose.onclick = function () {
+  UVLightModal.style.display = "none";
+}
+
+// What if We Wore Smart Clothes post
+var smartClothesModal = document.getElementById("smartClothesModal");
+var smartClothesBtn = document.getElementById("smartClothesBtn");
+var smartClothesClose = document.getElementById("smartClothesClose");
+
+smartClothesBtn.onclick = function () {
+  smartClothesModal.style.display = "block";
+}
+
+smartClothesClose.onclick = function () {
+  smartClothesModal.style.display = "none";
+}
+
+// Can we Live on Mars by 2026 post
+var liveOnMarsModal = document.getElementById("liveOnMarsModal");
+var liveOnMarsBtn = document.getElementById("liveOnMarsBtn");
+var liveOnMarsClose = document.getElementById("liveOnMarsClose");
+
+liveOnMarsBtn.onclick = function () {
+  liveOnMarsModal.style.display = "block";
+}
+
+liveOnMarsClose.onclick = function () {
+  liveOnMarsModal.style.display = "none";
+}
+
+// Can we Create Electric Skin post
+var electricSkinModal = document.getElementById("electricSkinModal");
+var electricSkinBtn = document.getElementById("electricSkinBtn");
+var electricSkinClose = document.getElementById("electricSkinClose");
+
+electricSkinBtn.onclick = function () {
+  electricSkinModal.style.display = "block";
+}
+
+electricSkinClose.onclick = function () {
+  electricSkinModal.style.display = "none";
+}
+
+// Should we Put 4G On the Moon post
+var FgOnMoonModal = document.getElementById("FgOnMoonModal");
+var FgOnMoonBtn = document.getElementById("FgOnMoonBtn");
+var FgOnMoonClose = document.getElementById("FgOnMoonClose");
+
+FgOnMoonBtn.onclick = function () {
+  FgOnMoonModal.style.display = "block";
+}
+
+FgOnMoonClose.onclick = function () {
+  FgOnMoonModal.style.display = "none";
+}
+
+// Have we Found a Cure for COVID-19 post
+var covidVaccineModal = document.getElementById("covidVaccineModal");
+var covidVaccineBtn = document.getElementById("covidVaccineBtn");
+var covidVaccineClose = document.getElementById("covidVaccineClose");
+
+covidVaccineBtn.onclick = function () {
+  covidVaccineModal.style.display = "block";
+}
+
+covidVaccineClose.onclick = function () {
+  covidVaccineModal.style.display = "none";
+}
+
+// 5G Myths Debunked post
+var FgMythsModal = document.getElementById("FgMythsModal");
+var FgMythsBtn = document.getElementById("FgMythsBtn");
+var FgMythsClose = document.getElementById("FgMythsClose");
+
+FgMythsBtn.onclick = function () {
+  FgMythsModal.style.display = "block";
+}
+
+FgMythsClose.onclick = function () {
+  FgMythsModal.style.display = "none";
+}
