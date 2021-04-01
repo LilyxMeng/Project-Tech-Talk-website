995410a5acd6ddd2a6c32a8b30b4a66e6005106a
Lily Meng <LilyxMeng@github.com>
2021-03-31 20:43:35 -0400

    added all posts (ik this is painful to look at too)

--- js/index.js
+++ js/index.js
@@ -64,6 +64,19 @@ UVLightClose.onclick = function () {
   UVLightModal.style.display = "none";
 }
 
+// Can We Live without Transistors post
+var transistorsModal = document.getElementById("transistorsModal");
+var transistorsBtn = document.getElementById("transistorsBtn");
+var transistorsClose = document.getElementById("transistorsClose");
+
+transistorsBtn.onclick = function () {
+  transistorsModal.style.display = "block";
+}
+
+transistorsClose.onclick = function () {
+  transistorsModal.style.display = "none";
+}
+
 // What if We Wore Smart Clothes post
 var smartClothesModal = document.getElementById("smartClothesModal");
 var smartClothesBtn = document.getElementById("smartClothesBtn");
@@ -141,3 +154,138 @@ FgMythsBtn.onclick = function () {
 FgMythsClose.onclick = function () {
   FgMythsModal.style.display = "none";
 }
+
+// Can We Prevent Power Outages (A Deep Dive into Smart Grids) post
+var smartGridsModal = document.getElementById("smartGridsModal");
+var smartGridsBtn = document.getElementById("smartGridsBtn");
+var smartGridsClose = document.getElementById("smartGridsClose");
+
+smartGridsBtn.onclick = function () {
+  smartGridsModal.style.display = "block";
+}
+
+smartGridsClose.onclick = function () {
+  smartGridsModal.style.display = "none";
+}
+
+// Presenting Big Data post
+var bigDataModal = document.getElementById("bigDataModal");
+var bigDataBtn = document.getElementById("bigDataBtn");
+var bigDataClose = document.getElementById("bigDataClose");
+
+bigDataBtn.onclick = function () {
+  bigDataModal.style.display = "block";
+}
+
+bigDataClose.onclick = function () {
+  bigDataModal.style.display = "none";
+}
+
+// Presenting Electric Cars post
+var electricCarsModal = document.getElementById("electricCarsModal");
+var electricCarsBtn = document.getElementById("electricCarsBtn");
+var electricCarsClose = document.getElementById("electricCarsClose");
+
+electricCarsBtn.onclick = function () {
+  electricCarsModal.style.display = "block";
+}
+
+electricCarsClose.onclick = function () {
+  electricCarsModal.style.display = "none";
+}
+
+// Introducing Smart Cities post
+var smartCitiesModal = document.getElementById("smartCitiesModal");
+var smartCitiesBtn = document.getElementById("smartCitiesBtn");
+var smartCitiesClose = document.getElementById("smartCitiesClose");
+
+smartCitiesBtn.onclick = function () {
+  smartCitiesModal.style.display = "block";
+}
+
+smartCitiesClose.onclick = function () {
+  smartCitiesModal.style.display = "none";
+}
+
+// Gene Editing post
+var geneEditingModal = document.getElementById("geneEditingModal");
+var geneEditingBtn = document.getElementById("geneEditingBtn");
+var geneEditingClose = document.getElementById("geneEditingClose");
+
+geneEditingBtn.onclick = function () {
+  geneEditingModal.style.display = "block";
+}
+
+geneEditingClose.onclick = function () {
+  geneEditingModal.style.display = "none";
+}
+
+// Open Source Tech post
+var openSourceTechModal = document.getElementById("openSourceTechModal");
+var openSourceTechBtn = document.getElementById("openSourceTechBtn");
+var openSourceTechClose = document.getElementById("openSourceTechClose");
+
+openSourceTechBtn.onclick = function () {
+  openSourceTechModal.style.display = "block";
+}
+
+openSourceTechClose.onclick = function () {
+  openSourceTechModal.style.display = "none";
+}
+
+// Disinfection Robots post
+var disinfectionRobotsModal = document.getElementById("disinfectionRobotsModal");
+var disinfectionRobotsBtn = document.getElementById("disinfectionRobotsBtn");
+var disinfectionRobotsClose = document.getElementById("disinfectionRobotsClose");
+
+disinfectionRobotsBtn.onclick = function () {
+  disinfectionRobotsModal.style.display = "block";
+}
+
+disinfectionRobotsClose.onclick = function () {
+  disinfectionRobotsModal.style.display = "none";
+}
+
+// Contact Tracing post
+var contactTracingModal = document.getElementById("contactTracingModal");
+var contactTracingBtn = document.getElementById("contactTracingBtn");
+var contactTracingClose = document.getElementById("contactTracingClose");
+
+contactTracingBtn.onclick = function () {
+  contactTracingModal.style.display = "block";
+}
+
+contactTracingClose.onclick = function () {
+  contactTracingModal.style.display = "none";
+}
+
+// Nanotechnology post
+var nanotechnologyModal = document.getElementById("nanotechnologyModal");
+var nanotechnologyBtn = document.getElementById("nanotechnologyBtn");
+var nanotechnologyClose = document.getElementById("nanotechnologyClose");
+
+nanotechnologyBtn.onclick = function () {
+  nanotechnologyModal.style.display = "block";
+}
+
+nanotechnologyClose.onclick = function () {
+  nanotechnologyModal.style.display = "none";
+}
+
+// bioprinting post
+var bioprintingModal = document.getElementById("bioprintingModal");
+var bioprintingBtn = document.getElementById("bioprintingBtn");
+var bioprintingClose = document.getElementById("bioprintingClose");
+
+bioprintingBtn.onclick = function () {
+  bioprintingModal.style.display = "block";
+}
+
+bioprintingClose.onclick = function () {
+  bioprintingModal.style.display = "none";
+}
+
+
+
+
+
