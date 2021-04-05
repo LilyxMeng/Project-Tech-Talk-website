867722ebfa0dde06bdb3fe426921fb03633e7e90
Lily Meng <LilyxMeng@github.com>
2021-04-04 23:54:01 -0400

    added filter function

--- js/index.js
+++ js/index.js
@@ -285,7 +285,35 @@ bioprintingClose.onclick = function () {
   bioprintingModal.style.display = "none";
 }
 
-
-
-
-
+// button filter
+filterSelection("all");
+function filterSelection(c) {
+   var x, i;
+   x = document.querySelectorAll(".post-card");
+   if (c == "all") c = "";
+   Array.from(x).forEach(item => {
+      removeActiveClass(item, "show");
+      if (item.className.indexOf(c) > -1) addActiveClass(item, "show");
+   });
+}
+function addActiveClass(ele, name) {
+   var i, arr1, arr2;
+   arr1 = ele.className.split(" ");
+   arr2 = name.split(" ");
+   for (i = 0; i < arr2.length; i++) {
+      if (arr1.indexOf(arr2[i]) == -1) {
+         ele.className += " " + arr2[i];
+      }
+   }
+}
+function removeActiveClass(ele, name) {
+   var i, arr1, arr2;
+   arr1 = ele.className.split(" ");
+   arr2 = name.split(" ");
+   for (i = 0; i < arr2.length; i++) {
+      while (arr1.indexOf(arr2[i]) > -1) {
+         arr1.splice(arr1.indexOf(arr2[i]), 1);
+      }
+   }
+   ele.className = arr1.join(" ");
+}
