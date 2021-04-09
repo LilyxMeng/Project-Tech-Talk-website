eeac768035fbb2b039a3091b82389c341ef959d1
Lily Meng <LilyxMeng@github.com>
2021-04-09 15:43:01 -0400

    added new post

--- js/index.js
+++ js/index.js
@@ -1,262 +1,16 @@
 // popups
-// Can AI Design Cars post
-var aiDesignCarsModal = document.getElementById("aiDesignCarsModal");
-var aiDesignCarsBtn = document.getElementById("aiDesignCarsBtn");
-var aiDesignCarsClose = document.getElementById("aiDesignCarsClose");
 
-aiDesignCarsBtn.onclick = function () {
-  aiDesignCarsModal.style.display = "block";
+// bioprinting post
+var bioprintingModal = document.getElementById("bioprintingModal");
+var bioprintingBtn = document.getElementById("bioprintingBtn");
+var bioprintingClose = document.getElementById("bioprintingClose");
+
+bioprintingBtn.onclick = function () {
+  bioprintingModal.style.display = "block";
 }
 
-aiDesignCarsClose.onclick = function () {
-  aiDesignCarsModal.style.display = "none";
-}
-
-// SpaceX's Latest Merlin Engines post
-var merlinEnginesModal = document.getElementById("merlinEnginesModal");
-var merlinEnginesBtn = document.getElementById("merlinEnginesBtn");
-var merlinEnginesClose = document.getElementById("merlinEnginesClose");
-
-merlinEnginesBtn.onclick = function () {
-  merlinEnginesModal.style.display = "block";
-}
-
-merlinEnginesClose.onclick = function () {
-  merlinEnginesModal.style.display = "none";
-}
-
-// How to Go Viral on TikTok post
-var tikTokModal = document.getElementById("tikTokModal");
-var tikToksBtn = document.getElementById("tikTokBtn");
-var tikTokClose = document.getElementById("tikTokClose");
-
-tikTokBtn.onclick = function () {
-  tikTokModal.style.display = "block";
-}
-
-tikTokClose.onclick = function () {
-  tikTokModal.style.display = "none";
-}
-
-// The Perseverance of Mankind post
-var perseveranceMankindModal = document.getElementById("perseveranceMankindModal");
-var perseveranceMankindBtn = document.getElementById("perseveranceMankindBtn");
-var perseveranceMankindClose = document.getElementById("perseveranceMankindClose");
-
-perseveranceMankindBtn.onclick = function () {
-  perseveranceMankindModal.style.display = "block";
-}
-
-perseveranceMankindClose.onclick = function () {
-  perseveranceMankindModal.style.display = "none";
-}
-
-// Can We Make UV Light Lisible post
-var UVLightModal = document.getElementById("UVLightModal");
-var UVLightBtn = document.getElementById("UVLightBtn");
-var UVLightClose = document.getElementById("UVLightClose");
-
-UVLightBtn.onclick = function () {
-  UVLightModal.style.display = "block";
-}
-
-UVLightClose.onclick = function () {
-  UVLightModal.style.display = "none";
-}
-
-// Can We Live without Transistors post
-var transistorsModal = document.getElementById("transistorsModal");
-var transistorsBtn = document.getElementById("transistorsBtn");
-var transistorsClose = document.getElementById("transistorsClose");
-
-transistorsBtn.onclick = function () {
-  transistorsModal.style.display = "block";
-}
-
-transistorsClose.onclick = function () {
-  transistorsModal.style.display = "none";
-}
-
-// What if We Wore Smart Clothes post
-var smartClothesModal = document.getElementById("smartClothesModal");
-var smartClothesBtn = document.getElementById("smartClothesBtn");
-var smartClothesClose = document.getElementById("smartClothesClose");
-
-smartClothesBtn.onclick = function () {
-  smartClothesModal.style.display = "block";
-}
-
-smartClothesClose.onclick = function () {
-  smartClothesModal.style.display = "none";
-}
-
-// Can we Live on Mars by 2026 post
-var liveOnMarsModal = document.getElementById("liveOnMarsModal");
-var liveOnMarsBtn = document.getElementById("liveOnMarsBtn");
-var liveOnMarsClose = document.getElementById("liveOnMarsClose");
-
-liveOnMarsBtn.onclick = function () {
-  liveOnMarsModal.style.display = "block";
-}
-
-liveOnMarsClose.onclick = function () {
-  liveOnMarsModal.style.display = "none";
-}
-
-// Can we Create Electric Skin post
-var electricSkinModal = document.getElementById("electricSkinModal");
-var electricSkinBtn = document.getElementById("electricSkinBtn");
-var electricSkinClose = document.getElementById("electricSkinClose");
-
-electricSkinBtn.onclick = function () {
-  electricSkinModal.style.display = "block";
-}
-
-electricSkinClose.onclick = function () {
-  electricSkinModal.style.display = "none";
-}
-
-// Should we Put 4G On the Moon post
-var FgOnMoonModal = document.getElementById("FgOnMoonModal");
-var FgOnMoonBtn = document.getElementById("FgOnMoonBtn");
-var FgOnMoonClose = document.getElementById("FgOnMoonClose");
-
-FgOnMoonBtn.onclick = function () {
-  FgOnMoonModal.style.display = "block";
-}
-
-FgOnMoonClose.onclick = function () {
-  FgOnMoonModal.style.display = "none";
-}
-
-// Have we Found a Cure for COVID-19 post
-var covidVaccineModal = document.getElementById("covidVaccineModal");
-var covidVaccineBtn = document.getElementById("covidVaccineBtn");
-var covidVaccineClose = document.getElementById("covidVaccineClose");
-
-covidVaccineBtn.onclick = function () {
-  covidVaccineModal.style.display = "block";
-}
-
-covidVaccineClose.onclick = function () {
-  covidVaccineModal.style.display = "none";
-}
-
-// 5G Myths Debunked post
-var FgMythsModal = document.getElementById("FgMythsModal");
-var FgMythsBtn = document.getElementById("FgMythsBtn");
-var FgMythsClose = document.getElementById("FgMythsClose");
-
-FgMythsBtn.onclick = function () {
-  FgMythsModal.style.display = "block";
-}
-
-FgMythsClose.onclick = function () {
-  FgMythsModal.style.display = "none";
-}
-
-// Can We Prevent Power Outages (A Deep Dive into Smart Grids) post
-var smartGridsModal = document.getElementById("smartGridsModal");
-var smartGridsBtn = document.getElementById("smartGridsBtn");
-var smartGridsClose = document.getElementById("smartGridsClose");
-
-smartGridsBtn.onclick = function () {
-  smartGridsModal.style.display = "block";
-}
-
-smartGridsClose.onclick = function () {
-  smartGridsModal.style.display = "none";
-}
-
-// Presenting Big Data post
-var bigDataModal = document.getElementById("bigDataModal");
-var bigDataBtn = document.getElementById("bigDataBtn");
-var bigDataClose = document.getElementById("bigDataClose");
-
-bigDataBtn.onclick = function () {
-  bigDataModal.style.display = "block";
-}
-
-bigDataClose.onclick = function () {
-  bigDataModal.style.display = "none";
-}
-
-// Presenting Electric Cars post
-var electricCarsModal = document.getElementById("electricCarsModal");
-var electricCarsBtn = document.getElementById("electricCarsBtn");
-var electricCarsClose = document.getElementById("electricCarsClose");
-
-electricCarsBtn.onclick = function () {
-  electricCarsModal.style.display = "block";
-}
-
-electricCarsClose.onclick = function () {
-  electricCarsModal.style.display = "none";
-}
-
-// Introducing Smart Cities post
-var smartCitiesModal = document.getElementById("smartCitiesModal");
-var smartCitiesBtn = document.getElementById("smartCitiesBtn");
-var smartCitiesClose = document.getElementById("smartCitiesClose");
-
-smartCitiesBtn.onclick = function () {
-  smartCitiesModal.style.display = "block";
-}
-
-smartCitiesClose.onclick = function () {
-  smartCitiesModal.style.display = "none";
-}
-
-// Gene Editing post
-var geneEditingModal = document.getElementById("geneEditingModal");
-var geneEditingBtn = document.getElementById("geneEditingBtn");
-var geneEditingClose = document.getElementById("geneEditingClose");
-
-geneEditingBtn.onclick = function () {
-  geneEditingModal.style.display = "block";
-}
-
-geneEditingClose.onclick = function () {
-  geneEditingModal.style.display = "none";
-}
-
-// Open Source Tech post
-var openSourceTechModal = document.getElementById("openSourceTechModal");
-var openSourceTechBtn = document.getElementById("openSourceTechBtn");
-var openSourceTechClose = document.getElementById("openSourceTechClose");
-
-openSourceTechBtn.onclick = function () {
-  openSourceTechModal.style.display = "block";
-}
-
-openSourceTechClose.onclick = function () {
-  openSourceTechModal.style.display = "none";
-}
-
-// Disinfection Robots post
-var disinfectionRobotsModal = document.getElementById("disinfectionRobotsModal");
-var disinfectionRobotsBtn = document.getElementById("disinfectionRobotsBtn");
-var disinfectionRobotsClose = document.getElementById("disinfectionRobotsClose");
-
-disinfectionRobotsBtn.onclick = function () {
-  disinfectionRobotsModal.style.display = "block";
-}
-
-disinfectionRobotsClose.onclick = function () {
-  disinfectionRobotsModal.style.display = "none";
-}
-
-// Contact Tracing post
-var contactTracingModal = document.getElementById("contactTracingModal");
-var contactTracingBtn = document.getElementById("contactTracingBtn");
-var contactTracingClose = document.getElementById("contactTracingClose");
-
-contactTracingBtn.onclick = function () {
-  contactTracingModal.style.display = "block";
-}
-
-contactTracingClose.onclick = function () {
-  contactTracingModal.style.display = "none";
+bioprintingClose.onclick = function () {
+  bioprintingModal.style.display = "none";
 }
 
 // Nanotechnology post
@@ -272,48 +26,310 @@ nanotechnologyClose.onclick = function () {
   nanotechnologyModal.style.display = "none";
 }
 
-// bioprinting post
-var bioprintingModal = document.getElementById("bioprintingModal");
-var bioprintingBtn = document.getElementById("bioprintingBtn");
-var bioprintingClose = document.getElementById("bioprintingClose");
+// Contact Tracing post
+var contactTracingModal = document.getElementById("contactTracingModal");
+var contactTracingBtn = document.getElementById("contactTracingBtn");
+var contactTracingClose = document.getElementById("contactTracingClose");
 
-bioprintingBtn.onclick = function () {
-  bioprintingModal.style.display = "block";
+contactTracingBtn.onclick = function () {
+  contactTracingModal.style.display = "block";
 }
 
-bioprintingClose.onclick = function () {
-  bioprintingModal.style.display = "none";
+contactTracingClose.onclick = function () {
+  contactTracingModal.style.display = "none";
+}
+
+// Disinfection Robots post
+var disinfectionRobotsModal = document.getElementById("disinfectionRobotsModal");
+var disinfectionRobotsBtn = document.getElementById("disinfectionRobotsBtn");
+var disinfectionRobotsClose = document.getElementById("disinfectionRobotsClose");
+
+disinfectionRobotsBtn.onclick = function () {
+  disinfectionRobotsModal.style.display = "block";
+}
+
+disinfectionRobotsClose.onclick = function () {
+  disinfectionRobotsModal.style.display = "none";
+}
+
+// Open Source Tech post
+var openSourceTechModal = document.getElementById("openSourceTechModal");
+var openSourceTechBtn = document.getElementById("openSourceTechBtn");
+var openSourceTechClose = document.getElementById("openSourceTechClose");
+
+openSourceTechBtn.onclick = function () {
+  openSourceTechModal.style.display = "block";
+}
+
+openSourceTechClose.onclick = function () {
+  openSourceTechModal.style.display = "none";
+}
+
+// Gene Editing post
+var geneEditingModal = document.getElementById("geneEditingModal");
+var geneEditingBtn = document.getElementById("geneEditingBtn");
+var geneEditingClose = document.getElementById("geneEditingClose");
+
+geneEditingBtn.onclick = function () {
+  geneEditingModal.style.display = "block";
+}
+
+geneEditingClose.onclick = function () {
+  geneEditingModal.style.display = "none";
+}
+
+// Introducing Smart Cities post
+var smartCitiesModal = document.getElementById("smartCitiesModal");
+var smartCitiesBtn = document.getElementById("smartCitiesBtn");
+var smartCitiesClose = document.getElementById("smartCitiesClose");
+
+smartCitiesBtn.onclick = function () {
+  smartCitiesModal.style.display = "block";
+}
+
+smartCitiesClose.onclick = function () {
+  smartCitiesModal.style.display = "none";
+}
+
+// Presenting Electric Cars post
+var electricCarsModal = document.getElementById("electricCarsModal");
+var electricCarsBtn = document.getElementById("electricCarsBtn");
+var electricCarsClose = document.getElementById("electricCarsClose");
+
+electricCarsBtn.onclick = function () {
+  electricCarsModal.style.display = "block";
+}
+
+electricCarsClose.onclick = function () {
+  electricCarsModal.style.display = "none";
+}
+
+// Presenting Big Data post
+var bigDataModal = document.getElementById("bigDataModal");
+var bigDataBtn = document.getElementById("bigDataBtn");
+var bigDataClose = document.getElementById("bigDataClose");
+
+bigDataBtn.onclick = function () {
+  bigDataModal.style.display = "block";
+}
+
+bigDataClose.onclick = function () {
+  bigDataModal.style.display = "none";
+}
+
+// Can We Prevent Power Outages (A Deep Dive into Smart Grids) post
+var smartGridsModal = document.getElementById("smartGridsModal");
+var smartGridsBtn = document.getElementById("smartGridsBtn");
+var smartGridsClose = document.getElementById("smartGridsClose");
+
+smartGridsBtn.onclick = function () {
+  smartGridsModal.style.display = "block";
+}
+
+smartGridsClose.onclick = function () {
+  smartGridsModal.style.display = "none";
+}
+
+// 5G Myths Debunked post
+var FgMythsModal = document.getElementById("FgMythsModal");
+var FgMythsBtn = document.getElementById("FgMythsBtn");
+var FgMythsClose = document.getElementById("FgMythsClose");
+
+FgMythsBtn.onclick = function () {
+  FgMythsModal.style.display = "block";
+}
+
+FgMythsClose.onclick = function () {
+  FgMythsModal.style.display = "none";
+}
+
+// Have we Found a Cure for COVID-19 post
+var covidVaccineModal = document.getElementById("covidVaccineModal");
+var covidVaccineBtn = document.getElementById("covidVaccineBtn");
+var covidVaccineClose = document.getElementById("covidVaccineClose");
+
+covidVaccineBtn.onclick = function () {
+  covidVaccineModal.style.display = "block";
+}
+
+covidVaccineClose.onclick = function () {
+  covidVaccineModal.style.display = "none";
+}
+
+// Should we Put 4G On the Moon post
+var FgOnMoonModal = document.getElementById("FgOnMoonModal");
+var FgOnMoonBtn = document.getElementById("FgOnMoonBtn");
+var FgOnMoonClose = document.getElementById("FgOnMoonClose");
+
+FgOnMoonBtn.onclick = function () {
+  FgOnMoonModal.style.display = "block";
+}
+
+FgOnMoonClose.onclick = function () {
+  FgOnMoonModal.style.display = "none";
+}
+
+// Can we Create Electric Skin post
+var electricSkinModal = document.getElementById("electricSkinModal");
+var electricSkinBtn = document.getElementById("electricSkinBtn");
+var electricSkinClose = document.getElementById("electricSkinClose");
+
+electricSkinBtn.onclick = function () {
+  electricSkinModal.style.display = "block";
+}
+
+electricSkinClose.onclick = function () {
+  electricSkinModal.style.display = "none";
+}
+
+// Can we Live on Mars by 2026 post
+var liveOnMarsModal = document.getElementById("liveOnMarsModal");
+var liveOnMarsBtn = document.getElementById("liveOnMarsBtn");
+var liveOnMarsClose = document.getElementById("liveOnMarsClose");
+
+liveOnMarsBtn.onclick = function () {
+  liveOnMarsModal.style.display = "block";
+}
+
+liveOnMarsClose.onclick = function () {
+  liveOnMarsModal.style.display = "none";
+}
+
+// What if We Wore Smart Clothes post
+var smartClothesModal = document.getElementById("smartClothesModal");
+var smartClothesBtn = document.getElementById("smartClothesBtn");
+var smartClothesClose = document.getElementById("smartClothesClose");
+
+smartClothesBtn.onclick = function () {
+  smartClothesModal.style.display = "block";
+}
+
+smartClothesClose.onclick = function () {
+  smartClothesModal.style.display = "none";
+}
+
+// Can We Live without Transistors post
+var transistorsModal = document.getElementById("transistorsModal");
+var transistorsBtn = document.getElementById("transistorsBtn");
+var transistorsClose = document.getElementById("transistorsClose");
+
+transistorsBtn.onclick = function () {
+  transistorsModal.style.display = "block";
+}
+
+transistorsClose.onclick = function () {
+  transistorsModal.style.display = "none";
+}
+
+// Can We Make UV Light Lisible post
+var UVLightModal = document.getElementById("UVLightModal");
+var UVLightBtn = document.getElementById("UVLightBtn");
+var UVLightClose = document.getElementById("UVLightClose");
+
+UVLightBtn.onclick = function () {
+  UVLightModal.style.display = "block";
+}
+
+UVLightClose.onclick = function () {
+  UVLightModal.style.display = "none";
+}
+
+// The Perseverance of Mankind post
+var perseveranceMankindModal = document.getElementById("perseveranceMankindModal");
+var perseveranceMankindBtn = document.getElementById("perseveranceMankindBtn");
+var perseveranceMankindClose = document.getElementById("perseveranceMankindClose");
+
+perseveranceMankindBtn.onclick = function () {
+  perseveranceMankindModal.style.display = "block";
+}
+
+perseveranceMankindClose.onclick = function () {
+  perseveranceMankindModal.style.display = "none";
+}
+
+// How to Go Viral on TikTok post
+var tikTokModal = document.getElementById("tikTokModal");
+var tikToksBtn = document.getElementById("tikTokBtn");
+var tikTokClose = document.getElementById("tikTokClose");
+
+tikTokBtn.onclick = function () {
+  tikTokModal.style.display = "block";
+}
+
+tikTokClose.onclick = function () {
+  tikTokModal.style.display = "none";
+}
+
+// SpaceX's Latest Merlin Engines post
+var merlinEnginesModal = document.getElementById("merlinEnginesModal");
+var merlinEnginesBtn = document.getElementById("merlinEnginesBtn");
+var merlinEnginesClose = document.getElementById("merlinEnginesClose");
+
+merlinEnginesBtn.onclick = function () {
+  merlinEnginesModal.style.display = "block";
+}
+
+merlinEnginesClose.onclick = function () {
+  merlinEnginesModal.style.display = "none";
+}
+
+// Can AI Design Cars post
+var aiDesignCarsModal = document.getElementById("aiDesignCarsModal");
+var aiDesignCarsBtn = document.getElementById("aiDesignCarsBtn");
+var aiDesignCarsClose = document.getElementById("aiDesignCarsClose");
+
+aiDesignCarsBtn.onclick = function () {
+  aiDesignCarsModal.style.display = "block";
+}
+
+aiDesignCarsClose.onclick = function () {
+  aiDesignCarsModal.style.display = "none";
+}
+
+// Warp Drive post
+var warpDriveModal = document.getElementById("warpDriveModal");
+var warpDriveBtn = document.getElementById("warpDriveBtn");
+var warpDriveClose = document.getElementById("warpDriveClose");
+
+warpDriveBtn.onclick = function () {
+  warpDriveModal.style.display = "block";
+}
+
+warpDriveClose.onclick = function () {
+  warpDriveModal.style.display = "none";
 }
 
 // button filter
 filterSelection("all");
 function filterSelection(c) {
-   var x, i;
-   x = document.querySelectorAll(".post-card");
-   if (c == "all") c = "";
-   Array.from(x).forEach(item => {
-      removeActiveClass(item, "show");
-      if (item.className.indexOf(c) > -1) addActiveClass(item, "show");
-   });
+  var x, i;
+  x = document.querySelectorAll(".post-card");
+  if (c == "all") c = "";
+  Array.from(x).forEach(item => {
+    removeActiveClass(item, "show");
+    if (item.className.indexOf(c) > -1) addActiveClass(item, "show");
+  });
 }
+
 function addActiveClass(ele, name) {
-   var i, arr1, arr2;
-   arr1 = ele.className.split(" ");
-   arr2 = name.split(" ");
-   for (i = 0; i < arr2.length; i++) {
-      if (arr1.indexOf(arr2[i]) == -1) {
-         ele.className += " " + arr2[i];
-      }
-   }
+  var i, arr1, arr2;
+  arr1 = ele.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      ele.className += " " + arr2[i];
+    }
+  }
 }
+
 function removeActiveClass(ele, name) {
-   var i, arr1, arr2;
-   arr1 = ele.className.split(" ");
-   arr2 = name.split(" ");
-   for (i = 0; i < arr2.length; i++) {
-      while (arr1.indexOf(arr2[i]) > -1) {
-         arr1.splice(arr1.indexOf(arr2[i]), 1);
-      }
-   }
-   ele.className = arr1.join(" ");
+  var i, arr1, arr2;
+  arr1 = ele.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  ele.className = arr1.join(" ");
 }
